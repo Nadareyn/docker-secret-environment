c40c27b211e4c17a4ba6bc915a9cafc2f374397e
Nadareyn <1404782+Nadareyn@users.noreply.github.com>
2020-01-05 21:15:38 +0100

    Replace part of variables

--- override.sh
+++ override.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+varit_key="VARIT_OVERRIDE__"
+vars=$(set | grep "=$varit_key" | grep -v "varit_key=$varit_key")
 files=$(ls /run/secrets/)
-vars=$(set)
-for file in $files; do
-    secret=$(tail /run/secrets/$file);
-    echo "echo \"Add ${file} variable\""
-    echo "export ${file}=${secret}"
-    for var in $vars; do
-      IFS='=' read -ra var_dico <<< "$var"
-      if [ "${var_dico[1]}" = "__${file}__" ]; then
-        echo "echo \"Replacing ${var_dico[0]} by secret ${file}\""
-        echo "export ${var_dico[0]}=${secret}"
-      fi
-    done
+for var in $vars; do
+  var_name=$(echo "$var" | sed 's/=.*//')
+  var_value=$(echo "$var" | sed 's/[^=]*=//')
+  var_value="${var_value//$varit_key/}"
+  echo "echo \"Replacing $var_name\""
+  for file in $files; do
+    secret=$(tail /run/secrets/$file)
+    var_value="${var_value//__${file}__/$secret}"
+  done
+
+  echo "export $var_name=$var_value"
 done
