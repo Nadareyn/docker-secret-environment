dbb0b7adf59907856524ebcd6348e268ea1ab895
Nadareyn <1404782+Nadareyn@users.noreply.github.com>
2020-01-05 13:05:52 +0100

    print command instead of execute this

--- override.sh
+++ override.sh
@@ -2,14 +2,14 @@
 
 files=$(ls /run/secrets/)
 vars=$(set)
-for file in $files; do 
+for file in $files; do
     secret=$(tail /run/secrets/$file);
-    export ${file}=${secret}
+    echo "export ${file}=${secret}"
     for var in $vars; do
       IFS='=' read -ra var_dico <<< "$var"
       if [ "${var_dico[1]}" = "__${file}__" ]; then
-        echo "Replacing ${var_dico[0]} by secret ${file}"
-        export ${var_dico[0]}=${secret}
+        echo "echo \"Replacing ${var_dico[0]} by secret ${file}\""
+        echo "export ${var_dico[0]}=${secret}"
       fi
     done
 done
