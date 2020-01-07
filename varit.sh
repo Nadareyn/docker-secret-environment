7df3c24fcca73b81434751dc92f87a64c2723464
Nadareyn <1404782+Nadareyn@users.noreply.github.com>
2020-01-07 20:58:38 +0100

    wrong condition

--- varit.sh
+++ varit.sh
@@ -5,6 +5,7 @@ file_dir=`dirname $0`
 if [ ! -f "/bin/bash" ]
 then
     apk --no-cache add bash
-    eval "$($file_dir/generate_export.sh)"
-    eval "$1"
 fi
+
+eval "$($file_dir/generate_export.sh)"
+eval "$1"
